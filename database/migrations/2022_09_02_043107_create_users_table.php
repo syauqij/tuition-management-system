5e989111b73dd786a00173f000ac677c62425057
Muhamad Syauqi Jamil <syauqi.j@outlook.com>
2022-09-02 13:24:10 +0800

    Create db migration table users

--- database/migrations/2022_09_02_043107_create_users_table.php
+++ database/migrations/2022_09_02_043107_create_users_table.php
@@ -14,11 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id()->autoIncrement();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['student', 'teacher', 'finance', 'admin']);
+            $table->string('phone_no', 15);
+            $table->boolean('is_active')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
