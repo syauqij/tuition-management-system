33b8a3adf858fc9433335eff8cef21a5cf0ceba9
Muhamad Syauqi Jamil <syauqi.j@outlook.com>
2022-11-24 01:32:34 +0800

    Fix component tags typo. Update layout for Profile Update.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,57 +4,76 @@
           {{ __('My Profile') }}
       </h2>
   </x-slot>
-
+  
   <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 bg-white border-b border-gray-200">
-                  @include('layouts.flash-message')
-                  <form method="POST" autocomplete="off" action="{{ route('profile.update') }}">
-                      @method('PUT')
-                      @csrf
-                      <div class="grid grid-cols-2 gap-2">
- 
-                        <div>
-                            <x-forms.input-label for="first_name" :value="__('First Name')" />
-                            <x-forms.input-text id="first_name" class="block mt-1 w-full" type="text" name="first_name" value="{{ auth()->user()->first_name }}" />
-                            @error('first_name')
-                              <x-alert-message type="error" :message="$message"/>
-                            @enderror
-                        </div>
-                        
-                        <div>
-                          <x-forms.input-label for="last_name" :value="__('Last Name')" />
-                          <x-forms.input-text id="last_name" class="block mt-1 w-full" type="text" name="last_name" value="{{ auth()->user()->last_name }}" />
-                            @error('last_name')
-                              <x-alert-message type="error" :message="$message"/>
-                            @enderror
-                          </div>
-                          
-                          <div>
-                            <x-forms.input-label for="email" :value="__('Email')" />
-                            <x-forms.input-text id="email" class="block mt-1 w-full" type="email" name="email" value="{{ auth()->user()->email }}" />
-                            @error('email')
-                              <x-alert-message type="error" :message="$message"/>
-                            @enderror
-                          </div>
+    <x-content.card>
 
-                          <div>
-                              <x-forms.input-label for="phone_no" :value="__('Phone Number')" />
-                              <x-forms.input-text id="phone_no" class="block mt-1 w-full" type="text" name="phone_no" value="{{ auth()->user()->phone_no }}" />
-                              @error('phone_no')
-                                <x-alert-message type="error" :message="$message"/>
-                              @enderror
-                          </div>
-                      </div>
-                      <div class="flex items-center justify-end mt-4">
-                          <x-forms.button-primary class="ml-3">
-                              {{ __('Update') }}
-                          </x-forms.button-primary>
-                      </div>
-                  </form>
-              </div>
-          </div>
+      <div class="flex flex-col text-center w-full mb-4">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">{{ auth()->user()->fullName }}</h1>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Please update your profile details below</p>
       </div>
+
+      <form method="POST" action="{{ route('profile.update', auth()->user()->id) }}" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="lg:w-1/2 md:w-2/3 mx-auto">
+          <div class="flex flex-wrap m-2">
+            
+            <div class="p-2 w-full">
+              <div class="relative">
+                <x-forms.input-label for="first_name" :value="__('First Name')" />
+                <x-forms.input-text id="first_name" class="block mt-1 w-full" type="text" name="first_name" value="{{ auth()->user()->first_name }}" />
+                @error('first_name')
+                  <x-alerts.message type="error" :message="$message"/>
+                @enderror
+              </div>
+            </div>
+            
+            <div class="p-2 w-full">
+              <div class="relative">
+                <x-forms.input-label for="last_name" :value="__('Last Name')" />
+                <x-forms.input-text id="last_name" class="block mt-1 w-full" type="text" name="last_name" value="{{ auth()->user()->last_name }}" />
+                  @error('last_name')
+                    <x-alerts.message type="error" :message="$message"/>
+                  @enderror
+              </div>
+            </div>
+            
+            <div class="p-2 w-full">
+              <div class="relative">
+                <x-forms.input-label for="email" :value="__('Email')" />
+                <x-forms.input-text id="email" class="block mt-1 w-full" type="email" name="email" value="{{ auth()->user()->email }}" />
+                @error('email')
+                  <x-alerts.message type="error" :message="$message"/>
+                @enderror
+              </div>
+            </div>
+            
+            <div class="p-2 w-full">
+              <div class="relative">
+                <x-forms.input-label for="phone_no" :value="__('Phone Number')" />
+                <x-forms.input-text id="phone_no" class="block mt-1 w-full" type="text" name="phone_no" value="{{ auth()->user()->phone_no }}" />
+                @error('phone_no')
+                  <x-alerts.message type="error" :message="$message"/>
+                @enderror
+              </div>
+            </div>
+
+            <div class="p-2 w-full flex justify-center">
+              <x-forms.button-cancel class="mr-2 text-lg" :value="__('dashboard')" >
+                {{ __('Cancel') }}
+              </x-forms.button-cancel>
+
+              <x-forms.button-primary class="text-lg">
+                {{ __('Update') }}
+              </x-forms.button-primary>
+            </div>
+
+          </div>
+        </div>
+      
+      </form>
+    </x-content.card>
+
   </div>
 </x-app-layout>
