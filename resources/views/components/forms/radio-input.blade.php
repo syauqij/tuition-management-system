4af2fec697bb6e9bee3d1104412a31718c167b34
Muhamad Syauqi Jamil <syauqi.j@outlook.com>
2022-11-27 23:26:45 +0800

    Fix retrieving default value for radio input component

--- resources/views/components/forms/radio-input.blade.php
+++ resources/views/components/forms/radio-input.blade.php
@@ -1,5 +1,7 @@
+@props(['selectedGender' => ''])
+
 <input {{ $attributes
   ->class(['w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500' 
     => $errors->has($attributes->get('name'))
   ])->merge(['disabled' => false])
-}} @checked(old($attributes->get('name')) == $attributes->get('value')) />
+}} @checked($attributes->get('value') == $selectedGender) />
