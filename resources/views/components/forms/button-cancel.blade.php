9ef23ce2255e7d373c85c69482cc21d27365e61c
Syauqi Jamil <syauqi.j@outlook.com>
2022-11-29 15:17:10 +0800

    Update classes for button cancel component

--- resources/views/components/forms/button-cancel.blade.php
+++ resources/views/components/forms/button-cancel.blade.php
@@ -1,7 +1,10 @@
 @props(['value'])
 
 <a href={{ route($value) }} {{$attributes}}>
-  <button type='button' class='text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg'>
+  <button type='button' class='inline-block px-6 py-2 mr-2 bg-gray-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md
+  hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0
+  active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out ripple-surface-light
+  '>
     {{ $slot }}
   </button>
 </a>
