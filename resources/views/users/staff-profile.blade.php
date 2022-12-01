c4c3a0a45534c652579fd91de6e484bf78b6feb6
Syauqi Jamil <syauqi.j@outlook.com>
2022-12-02 05:00:03 +0800

    Fix form inputs not displaying Staff Profile's values

--- resources/views/users/staff-profile.blade.php
+++ resources/views/users/staff-profile.blade.php
@@ -41,7 +41,7 @@
           <div class="md:col-span-2">
             <x-forms.input-label for="mykad" :value="__('MyKad')" />
             <x-forms.input-text id="mykad" class="block mt-1 w-full" type="number" name="mykad"
-              value="{{ $user->studentProfile->mykad ?? old('mykad')}} " />
+              value="{{ $user->staffProfile->mykad ?? old('mykad')}} " />
             @error('mykad')
               <x-alerts.message type="error" :message="$message"/>
             @enderror
@@ -50,7 +50,7 @@
           <div class="md:col-span-2">
             <x-forms.input-label for="birthdate" :value="__('Birthdate')" />
             <x-forms.input-text id="birthdate" class="block mt-1 w-full" type="date" name="birthdate"
-              value="{{ $user->studentProfile->birthdate ?? old('birthdate')  }}" />
+              value="{{ $user->staffProfile->birthdate ?? old('birthdate')  }}" />
             @error('birthdate')
               <x-alerts.message type="error" :message="$message"/>
             @enderror
@@ -61,13 +61,13 @@
             <ul class="grid gap-2 grid-cols-2 mb-2">
                 <li>
                     <x-forms.radio-input id="gender_male" class="hidden peer" type="radio" name="gender"
-                      :selectedGender="$user->studentProfile->gender ?? old('gender')"
+                      :selectedGender="$user->staffProfile->gender ?? old('gender')"
                       value="male" autofocus />
                     <x-forms.radio-label class="text-center" for="gender_male" :value="__('Male')" />
                 </li>
                 <li>
                     <x-forms.radio-input id="gender_female" class="hidden peer" type="radio" name="gender"
-                      :selectedGender="$user->studentProfile->gender ?? old('gender')"
+                      :selectedGender="$user->staffProfile->gender ?? old('gender')"
                       value="female" autofocus />
                     <x-forms.radio-label class="text-center" for="gender_female" :value="__('Female')" />
                 </li>
@@ -80,7 +80,7 @@
           <div class="md:col-span-5">
             <x-forms.input-label for="street_1" :value="__('Address / Street 1')" />
             <x-forms.input-text id="street_1" class="block mt-1 w-full" type="text" name="street_1"
-              value="{{ $user->studentProfile->street_1 ?? old('street_1') }}"  />
+              value="{{ $user->staffProfile->street_1 ?? old('street_1') }}"  />
               @error('street_1')
                 <x-alerts.message type="error" :message="$message"/>
               @enderror
@@ -89,7 +89,7 @@
           <div class="md:col-start-1 md:col-span-5">
             <x-forms.input-label for="street_2" :value="__('Street 2')" />
             <x-forms.input-text id="street_2" class="block mt-1 w-full" type="text" name="street_2"
-              value="{{ $user->studentProfile->street_2 ?? old('street_2')}}" />
+              value="{{ $user->staffProfile->street_2 ?? old('street_2')}}" />
               @error('street_2')
                 <x-alerts.message type="error" :message="$message"/>
               @enderror
@@ -98,7 +98,7 @@
           <div class="md:col-span-1">
             <x-forms.input-label for="postocde" :value="__('Postcode')" />
             <x-forms.input-text id="postocde" class="block mt-1 w-full" type="text" name="postcode"
-              value="{{ $user->studentProfile->postcode ?? old('postcode') }}" />
+              value="{{ $user->staffProfile->postcode ?? old('postcode') }}" />
               @error('postcode')
                 <x-alerts.message type="error" :message="$message"/>
               @enderror
@@ -107,7 +107,7 @@
           <div class="md:col-span-2">
             <x-forms.input-label for="city" :value="__('City')" />
             <x-forms.input-text id="city" class="block mt-1 w-full" type="text" name="city"
-              value="{{ $user->studentProfile->city ?? old('city') }}" />
+              value="{{ $user->staffProfile->city ?? old('city') }}" />
               @error('city')
                 <x-alerts.message type="error" :message="$message"/>
               @enderror
@@ -116,7 +116,7 @@
           <div class="md:col-span-2">
             <x-forms.input-label for="state" :value="__('State')" />
             <x-forms.input-text id="state" class="block mt-1 w-full" type="text" name="state"
-              value="{{ $user->studentProfile->state ?? old('state') }}" />
+              value="{{ $user->staffProfile->state ?? old('state') }}" />
               @error('state')
                 <x-alerts.message type="error" :message="$message"/>
               @enderror
@@ -125,7 +125,7 @@
           <div class="md:col-span-2">
             <x-forms.input-label for="country" :value="__('Country')" />
             <x-forms.input-text id="country" class="block mt-1 w-full" type="text" name="country"
-              value="{{ $user->studentProfile->country ?? old('country') }}" />
+              value="{{ $user->staffProfile->country ?? old('country') }}" />
               @error('country')
                 <x-alerts.message type="error" :message="$message" />
               @enderror
