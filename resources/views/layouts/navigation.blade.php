ed3201787ad49c18502857e419ef6a7117883487
Syauqi Jamil <syauqi.j@outlook.com>
2022-12-02 21:00:35 +0800

    Fix navigation menu not active

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
                     <x-nav.link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav.link>
-                    <x-nav.link :href="route('settings')" :active="request()->routeIs('settings') || request()->is('settings/*')">
+                    <x-nav.link :href="route('settings')" :active="request()->is('settings*')">
                         {{ __('Settings') }}
                     </x-nav.link>
                 </div>
