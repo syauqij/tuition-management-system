53befcc67e40ffc0d491bdbe4a9e8ea3b34cf06c
Syauqi Jamil <syauqi.j@outlook.com>
2023-01-10 21:07:47 +0800

    Create migration change street_2 to nullable

---
+++ database/migrations/2023_01_10_205019_make_street_2_nullable_on_student_profile_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  public function up()
+  {
+      Schema::table('student_profiles', function (Blueprint $table) {
+          $table->string('street_2')->nullable()->change();
+      });
+  }
+
+  public function down()
+  {
+      Schema::table('student_profiles', function (Blueprint $table) {
+          $table->string('street_2')->nullable(false)->change();
+      });
+  }
+};
